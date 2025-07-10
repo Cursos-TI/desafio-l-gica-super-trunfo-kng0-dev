e914b135b86e832ab9c3a119b23b306302f91d37
Igor Duarte <77506924+kng0-dev@users.noreply.github.com>
2025-07-10 17:54:44 +0000

    insere variaveis char

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -39,14 +39,17 @@ int main() {
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
-
-     int codigoCarta1, codigoCarta2, numPntsTuristicos1, numPntsTuristicos2;
+    char codigoCarta1[10], codigoCarta2[10], estado1[1], estado2[1], nomeCidade1[50], nomeCidade2[50];
+    int numPntsTuristicos1, numPntsTuristicos2;
     float area1, area2, pib1, pib2;
     float densidadePopulacional1,densidadePopulacional2, pibPerCapita1,pibPerCapita2;
     unsigned long int populacao1, populacao2;
     float superPoder1, superPoder2;
     
     printf("Carta 01:\n"); //insere as informações da primeira carta
+    printf("Informe o código da carta: \n");
+    scanf("%i", &codigoCarta1);
+
     printf("Informe a população da cidade: \n");
     scanf("%lu", &populacao1);
     printf("Informe a área da cidade: \n");
@@ -72,6 +75,6 @@ int main() {
 
     //disponibiliza os quadros com as informações das cartas
     printf("\n\nCarta 1:\nEstado: A\nCódigo: A01\nNome da Cidade: Belo Horizonte\nPopulação: %lu\nÁrea: %.2fkm²\nPIB: R$%.2f\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n\nCarta 2:\nEstado: B\nCódigo: B03\nNome da Cidade: Ilhéus\nPopulação: %lu\nÁrea: %.2fkm²\nPIB: R$%.2f\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n\n", populacao1, area1, pib1, numPntsTuristicos1,densidadePopulacional1,pibPerCapita1, populacao2, area2, pib2, numPntsTuristicos2,densidadePopulacional2,pibPerCapita2 ); 
-    
+
     return 0;
 }
